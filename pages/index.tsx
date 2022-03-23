41210024f0b0abcde10d238e0aa1480c522b9feb
IgorRoth <igorroth@unochapeco.edu.br>
2022-03-22 21:17:45 -0300

    install and configure tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,9 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <h1>Naruto Databook</h1>
+    <h1 className="text-9xl font-bold underline">
+      Hello Charuto!
+    </h1>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./pages/**/*.tsx",
+    "./components/**/*.tsx",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
